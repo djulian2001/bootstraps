d830a0c80414af2dcfa8a00913d11cd367092247
David Julian <david.julian@asu.edu>
2017-07-12 17:22:21 -0700

    Adding the work for the bootstraping project

---
+++ tests.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+/bin/echo "Test SSH with no password for deploy user."
+
+/bin/echo "Test Ansible can reach each remote hosts"
+
+/bin/echo "Test Ansible can fact check"
+
+/bin/echo "Test redis is caching"
+
+/bin/echo "Test Ansible works."
+
+/bin/echo "Test Ansible works."
