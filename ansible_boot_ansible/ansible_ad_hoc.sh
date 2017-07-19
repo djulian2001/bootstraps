8a26a54f8298710f3ad33ca1dfbeb023f247e9f6
David Julian <david.julian@asu.edu>
2017-07-18 17:36:16 -0700

    working copy of a playbook to bootstrap a privileged user (ansible) to all hosts defined.  Continued development will be required

--- ansible_boot_ansible/ansible_ad_hoc.sh
+++ ansible_boot_ansible/ansible_ad_hoc.sh
@@ -2,3 +2,16 @@
 ansible all -i hosts -u vagrant -k -m ping
 
 ansible all -i hosts -u vagrant -k -a "grep os_deployer /etc/group"
+
+
+# running the playbook from the ansible_boot_ansible directory
+ansible-playbook -k -u "vagrant" boot_test.yaml -e "admin_user=primusdj"
+
+# generate the passwd
+openssl passwd -1 ansible
+
+# this worked for our purpose
+
+# OR
+
+echo -n ansible | sha256sum | awk '{print $1}'
